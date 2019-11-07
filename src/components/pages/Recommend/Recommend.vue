d4277ebae5d4732cae48c8827a0749f6ac2e0e97
dingdazhuang <1021797587@qq.com>
2019-11-07 17:40:52 +0800

    'all'

--- src/components/pages/Recommend/Recommend.vue
+++ src/components/pages/Recommend/Recommend.vue
@@ -3,11 +3,50 @@
     <div class="left-search">
       <h1>高考选科(7选3)及分数</h1>
       <div class="check-box">
-        <input type="checkbox">
+        <div class="check" v-for="(checkbox, index) in checkboxes" :key="index">
+          <input type="checkbox" :checked="checkbox.checked"> <label for="">{{checkbox.text}}</label>
+        </div>
+      </div>
+      <div class="input-box">
+        <div class="search-box">
+          <input type="text" v-model="score"><button @click="search">查询</button>
+        </div>
+        <ul class="num-list">
+          <li v-for="num in 10" :key="num"> {{10 - num}}</li>
+        </ul>
       </div>
     </div>
     <div class="right-content">
-
+      <div class="top">
+        <div class="tit">
+          <h1>填报建议</h1>
+          <ul class="label">
+            <li>拼运气</li>
+          </ul>
+        </div>
+        <div class="total">
+          共为你推荐<span>250个</span>个专业
+        </div>
+      </div>
+      <div class="main">
+        <ul class="result-list">
+          <li>
+            <router-link>
+              <div class="pic">
+                <img src="" alt="">
+              </div>
+              <div class="info1">
+                <div class="uniName">浙江大学</div>
+                <div class="major">计算机与科学</div>
+              </div>
+              <div class="info2">
+                <div class="address">浙江杭州</div>
+                <div class="low-score">2018录取最低分数：<span>628</span></div>
+              </div>
+            </router-link>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -16,7 +55,19 @@ export default {
   name: '',
   data () {
     return {
-      checkboxes: [{ checked: false,  }]
+      checkboxes: [{ checked: false, text: '物理' }, { checked: false, text: '化学' }, { checked: false, text: '生物' }, { checked: false, text: '政治' },
+      { checked: false, text: '历史' }, { checked: false, text: '地理' }, { checked: true, text: '技术' }],
+      score: null,
+      checkedList: []
+
+    }
+  },
+  methods: {
+    search () {
+      if (!this.score) {
+        alert('请输入分数')
+        return;
+      }
     }
   }
 }
@@ -26,5 +77,26 @@ export default {
 <style lang="scss" scoped>
 .recommend {
   color:#fff;
+  display: flex;
+  .left-search {
+    width: 350px;
+    padding: 0 32px;
+    border-left: 1px solid rgba(255, 255, 255, .1);
+    border-right: 1px solid rgba(255, 255, 255, .1);
+    h1{
+      font-size: 24px;
+    }
+    .check-box{
+      display: flex;
+      flex-wrap: wrap;
+      input[type=checkbox]{
+        width:26px;
+        height: 26px;
+        border-radius: 4px;
+        border: 1px solid #fff;
+        background: transparent;
+      }
+    }
+  }
 }
 </style>
